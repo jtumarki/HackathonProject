1d097b0582b44a92b19c442aef1336e1ffa3830a
Elizabeth Li <etli@andrew.cmu.edu>
2013-01-26 17:14:44 -0500

    Matches page

--- matches.php
+++ matches.php
@@ -2,34 +2,33 @@
 <?php include 'layout/header.php' ?>
 <!-- BEGIN PAGE CONTENT -->
 <ol>
-	<li class = "match">
-		<div class = "left">
+	<li class="match">
+		<img src="img/user-big.png" />
+		<div class="info">
+			<h1>Elizabeth Li</h1>
+			<a class="email" href="mailto:etli@andrew.cmu.edu">etli@andrew.cmu.edu</a>
 		</div>
-		<div class = "right">
-			<div class = "top"> 
-				<img src = "user.png" />
-				<h1>Name</h1>
-				<h2>Email</h2>
-			</div>
-			<div class = "bottom">
-				<h1>Common Interests:</h1>
-				<p></p>
-			</div>
+		<div class="interests">
+			<h2>Common interests with you:</h2>
+			<ul>
+			  <li class="tag">a cappella</li>
+			  <li class="tag">digital art</li>
+			  <li class="tag">Ellie Goulding</li>
+			</ul>
 		</div>
 	</li>
-	<li class = "match">
-		<div class = "left">
+	<li class="match">
+		<img src="img/user-big.png" />
+		<div class="info">
+			<h1>Jessica Tumarkin</h1>
+			<a class="email" href="mailto:jtumarki@andrew.cmu.edu">jtumarki@andrew.cmu.edu</a>
 		</div>
-		<div class = "right">
-			<div class = "top"> 
-				<img src = "user.png" />
-				<h1>Name</h1>
-				<h2>Email</h2>
-			</div>
-			<div class = "bottom">
-				<h1>Common Interests:</h1>
-				<p></p>
-			</div>
+		<div class="interests">
+			<h2>Common interests with you:</h2>
+			<ul>
+			  <li class="tag">kpop</li>
+			  <li class="tag">horseback riding</li>
+			</ul>
 		</div>
 	</li>
 </ol>
