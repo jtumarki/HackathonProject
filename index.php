f7a047a6dff1bbe195407187bef65fbc296c7a1e
Elizabeth Li <etli@andrew.cmu.edu>
2013-01-26 04:40:43 -0500

    Pretty things

--- index.php
+++ index.php
@@ -3,7 +3,8 @@
   <head>
   	<meta charset="utf-8">
   	<meta name="viewport" content="width=device-width">
-  	<title>SisterMatch</title>	
+  	<title>SisterMatch</title>
+  	<link rel='stylesheet' type='text/css' href='http://fonts.googleapis.com/css?family=Lato|Sacramento' />
   	<link rel="stylesheet" type="text/css" href="css/normalize.css" media="screen" />
   	<link rel="stylesheet" type="text/css" href="css/style.css" media="screen" />
   </head>
@@ -15,7 +16,7 @@
       <input type="password" name="password" placeholder="Password" />
       <input type="submit" value="Log In" /> or <a href="register.php">Register</a>
       
-      <br /><br /><a href="home.php">Click here to skip logging in</a>
+      <br /><br /><a href="home.php">(Click here to skip logging in)</a>
     </form>
     
     <script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min.js"></script>
