ab885e2dea097b6a15acd79ac734fd147283caea
Amy Lee <amylee@andrew.cmu.edu>
2013-01-26 15:56:49 -0500

    I did something cool

--- matches.php
+++ matches.php
@@ -1,8 +1,38 @@
 <?php $thisPage="Matches"; ?>
 <?php include 'layout/header.php' ?>
 <!-- BEGIN PAGE CONTENT -->
-
-<p>Hi I'm really tired</p>
+<ol>
+	<li class = "match">
+		<div class = "left">
+		</div>
+		<div class = "right">
+			<div class = "top"> 
+				<img src = "user.png" />
+				<h1>Name</h1>
+				<h2>Email</h2>
+			</div>
+			<div class = "bottom">
+				<h1>Common Interests:</h1>
+				<p></p>
+			</div>
+		</div>
+	</li>
+	<li class = "match">
+		<div class = "left">
+		</div>
+		<div class = "right">
+			<div class = "top"> 
+				<img src = "user.png" />
+				<h1>Name</h1>
+				<h2>Email</h2>
+			</div>
+			<div class = "bottom">
+				<h1>Common Interests:</h1>
+				<p></p>
+			</div>
+		</div>
+	</li>
+</ol>
 
 <!-- END PAGE CONTENT -->
 <?php include 'layout/footer.php' ?>
